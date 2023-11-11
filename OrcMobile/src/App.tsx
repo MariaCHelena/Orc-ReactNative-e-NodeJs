2326606126b3ea5517b5773697e7dbabcb89c4a9
Maria <marianenacarvalho@gmail.com>
2023-11-11 16:51:41 -0300

    feat: insert styled-components

--- OrcMobile/src/App.tsx
+++ OrcMobile/src/App.tsx
@@ -1,9 +1,19 @@
 import { Text, SafeAreaView, View } from "react-native"
+import styled from "styled-components/native"
+
+const TextNew = styled.Text`
+    font-size: 30px;
+    color: pink;
+    text-decoration: underline;
+`;
 
 const App = () => {
     return(
         <SafeAreaView>
             <Text>Testando</Text>
+            <TextNew>
+                NOVO TESTE
+            </TextNew>
         </SafeAreaView>
     )
 }
